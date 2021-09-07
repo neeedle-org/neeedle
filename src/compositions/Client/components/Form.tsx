6bdb188d77e7e5209892fb4af2b0c74a7799f735
Shoya <touka.nt.gh@gmail.com>
2021-09-08 01:13:11 +0900

    emphasize empty field on call

--- src/compositions/Client/components/Form.tsx
+++ src/compositions/Client/components/Form.tsx
@@ -18,7 +18,12 @@ export const Form: VFC<{
   const { open } = useWalletModal()
   const { settings } = useSettingsStore()
   const methods = useForm()
-  const { handleSubmit, register } = methods
+  const {
+    handleSubmit,
+    register,
+    clearErrors,
+    formState: { errors, isSubmitSuccessful },
+  } = methods
   const [output, setOutput] = useState<any>()
   const [errorMessage, setErrorMessage] = useState('')
   return (
@@ -51,6 +56,7 @@ export const Form: VFC<{
                   fieldType="uint256"
                   doc="transferring amount"
                   unit={settings.unit}
+                  hasError={!isSubmitSuccessful && !!errors['value']}
                   {...register(`value`, { required: true })}
                 />
               )}
@@ -63,6 +69,10 @@ export const Form: VFC<{
                     label={name}
                     fieldType={each.type}
                     doc={doc?.params?.[name]}
+                    hasError={
+                      !isSubmitSuccessful &&
+                      !!(errors[`args`] && errors[`args`][idx])
+                    }
                     {...register(`args[${idx}]`, { required: true })}
                   />
                 )
@@ -107,19 +117,20 @@ type InputProps = {
   fieldType: FieldType
   doc?: string
   unit?: typeof UNITS[number]['value']
+  hasError?: boolean
 } & InputHTMLAttributes<HTMLInputElement>
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ label, fieldType, doc, unit, ...props }, ref) => (
-    <label>
+  ({ label, fieldType, doc, unit, hasError, ...props }, ref) => (
+    <InputLabel hasError={hasError}>
       <div>
         {label}: <Type>{fieldType}</Type>
         <Doc>{doc}</Doc>
       </div>
       <div>
-        <input {...props} ref={ref} />
+        <input {...props} ref={ref} placeholder={hasError ? 'Required' : ''} />
         {unit && <Unit>{unitLabel(unit)}</Unit>}
       </div>
-    </label>
+    </InputLabel>
   ),
 )
 
@@ -176,25 +187,31 @@ const RawResponse = styled.details`
     margin-top: 0;
   }
 `
+const InputLabel = styled.label<{ hasError?: boolean }>`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  color: ${({ hasError, theme: { primary, error } }) =>
+    hasError ? error : primary};
+  input {
+    color: ${({ theme: { primary } }) => primary};
+    border: 1px solid;
+    padding: 4px 8px;
+  }
+  > * {
+    width: 100%;
+    max-width: 360px;
+  }
+`
+
 const Inputs = styled.div`
   display: flex;
   padding: 12px 56px 24px;
   flex-direction: column;
-  label {
-    width: 100%;
+  ${InputLabel} {
     margin-top: 8px;
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-start;
-    flex-wrap: wrap;
-    input {
-      border: 1px solid;
-      padding: 4px 8px;
-    }
-    > * {
-      width: 100%;
-      max-width: 360px;
-    }
   }
   ${Doc} {
     margin: 0 8px;
@@ -205,6 +222,7 @@ const Inputs = styled.div`
     ${ctaStyle};
   }
 `
+
 const NoParams = styled.label`
   opacity: 0.75;
   font-style: italic;
