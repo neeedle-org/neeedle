745677b8ba745df88904d89b20107a5c8f6b087f
Shoya <touka.nt.gh@gmail.com>
2021-09-08 18:50:55 +0900

    fix

--- src/utils/urlParser.ts
+++ src/utils/urlParser.ts
@@ -7,4 +7,4 @@ const GITHUB_URL = 'https://github.com'
 const GITHUB_CONTENT_URL = 'https://raw.githubusercontent.com'
 const isGithubUrl = (url: string) => url.startsWith(GITHUB_URL)
 const githubUrlToContent = (url: string) =>
-  url.replace(GITHUB_URL, GITHUB_CONTENT_URL).replace('blob', '')
+  url.replace(GITHUB_URL, GITHUB_CONTENT_URL).replace('/blob', '')
