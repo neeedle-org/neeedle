87d5e0c471a77558d05d3cf7f040c0198d13f8c6
Shoya-Y <touka.nt.gh@gmail.com>
2021-11-28 23:54:12 +0900

    fix

--- src/compositions/Client/Settings/ChainForm.tsx
+++ src/compositions/Client/Settings/ChainForm.tsx
@@ -12,7 +12,6 @@ type ChainFormProps = {
   replaceQueryParam: (puts: { key: QueryParamKey; value: string }[]) => void
 }
 export const ChainForm: VFC<ChainFormProps> = ({ replaceQueryParam }) => {
-  const [customChainId, setCustomChainId] = useState('')
   const { settings, setSettings } = useSettingsStore()
   const { chains, setChains } = useChainsStore()
   useEffect(() => {
@@ -20,67 +19,100 @@ export const ChainForm: VFC<ChainFormProps> = ({ replaceQueryParam }) => {
       res.json().then(setChains),
     )
   }, [])
+  const chain = chains.find((each) => each.chainId === settings.chainId)
   return (
-    <ChainFormItem title="Chain" output={''} errorMessage={''}>
-      <ChainControl>
-        <div>
-          <h4>
-            Select{' '}
-            <span>
-              (Thanks to:{' '}
-              <Link href={'https://chainid.network'}>
-                https://chainid.network
-              </Link>
-              )
-            </span>
-          </h4>
-          <select
-            onChange={({ target: { value } }) => {
-              setSettings({ chainId: +value })
-              replaceQueryParam([{ key: 'chainId', value }])
-            }}
-            value={settings.chainId}
-          >
-            <option />
-            {chains.map(({ name, chainId }) => (
-              <option key={chainId} value={chainId} label={name}>
-                {name}
-              </option>
-            ))}
-          </select>
-        </div>
-        or
-        <div>
-          <h4>Input</h4>
-          <input
-            value={customChainId}
-            onChange={({ target: { value } }) => setCustomChainId(value)}
-            type="number"
-          />
-        </div>
-        <button
-          onClick={() => {
-            setSettings({ chainId: +customChainId })
-            replaceQueryParam([{ key: 'chainId', value: customChainId }])
-          }}
-          disabled={!customChainId || !Number.isInteger(+customChainId)}
-        >
-          Set
-        </button>
-      </ChainControl>
+    <ChainFormItem
+      title="Chain"
+      output={
+        settings.chainId != null
+          ? `${chain?.name || 'Custom'}: ${settings.chainId}`
+          : ''
+      }
+      errorMessage={''}
+    >
+      <ChainControlForm
+        chains={chains}
+        onChange={(chainId) => {
+          console.log(chainId)
+          setSettings({ chainId })
+          replaceQueryParam([{ key: 'chainId', value: `${chainId || ''}` }])
+        }}
+      />
     </ChainFormItem>
   )
 }
+const ChainControlForm: VFC<{
+  chains: Chain[]
+  onChange: (chainId: number | undefined) => void
+}> = ({ chains, onChange }) => {
+  const [customChainId, setCustomChainId] = useState('')
+  const [selectedChainId, setSelectedChainId] = useState('')
+  return (
+    <ChainControl>
+      <div>
+        <h4>
+          Select{' '}
+          <span>
+            (Thanks to:{' '}
+            <Link href={'https://chainid.network'}>
+              https://chainid.network
+            </Link>
+            )
+          </span>
+        </h4>
+        <select
+          onChange={({ target: { value } }) => {
+            setSelectedChainId(value)
+          }}
+          value={selectedChainId}
+          disabled={!!customChainId}
+        >
+          <option />
+          {chains.map(({ name, chainId }) => (
+            <option key={chainId} value={chainId} label={name}>
+              {name}
+            </option>
+          ))}
+        </select>
+      </div>
+      or
+      <div>
+        <h4>Input</h4>
+        <input
+          value={customChainId}
+          onChange={({ target: { value } }) => setCustomChainId(value)}
+          type="number"
+        />
+      </div>
+      <button
+        onClick={() => {
+          const changed = customChainId !== '' ? customChainId : selectedChainId
+          console.log(changed)
+          onChange(changed != null && changed !== '' ? +changed : undefined)
+        }}
+      >
+        Set
+      </button>
+    </ChainControl>
+  )
+}
+
 const ChainControl = styled(Control)`
   align-items: flex-end;
   > div {
     flex: 1;
+    a {
+      text-decoration: underline;
+    }
     h4 {
       margin-left: 6px;
       margin-bottom: 8px;
     }
     select {
       width: 100%;
+      :disabled {
+        background: ${({ theme: { bgCodeBlock } }) => bgCodeBlock}80;
+      }
     }
     input {
       display: block;
